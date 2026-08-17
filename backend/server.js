ee742b2580e90e7cbd0f45e7f37c49e0588dc079
Saurav Suman <sauravs95088@gmail.com>
2026-08-18 01:10:28 +0530

    API and Cros

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,28 @@
 const express = require("express");
+const cors = require("cors");
 const app= express();
 const PORT = 5000;
-app.get('/',(req,res)=>{res.send('backend server is running')});
+app.use(cors());
+const products=[
+    {
+        id:1,
+        name:"samsung",
+        price:80000
+    },
+    {
+        id:2,
+        name:"lg",
+        price:60000
+    },
+    {
+        id:3,
+        name:"index",
+        price:50000
+    }
+];
+app.get("/api/products",(req,res)=>{
+    res.json(products);
+});
+app.get("/",(req,res)=>{res.send('backend server is running')});
 app.listen(PORT,()=>{console.log(`server is running on the port ${PORT}`)});
+
