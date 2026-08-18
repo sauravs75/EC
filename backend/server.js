46b05050558b67576bfa5c02482611f86faf7751
Saurav Suman <sauravs95088@gmail.com>
2026-08-19 02:42:08 +0530

    Day3

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const app= express();
 const PORT = 5000;
 app.use(cors());
+app.use(express.json());
 const products=[
     {
         id:1,
@@ -22,6 +23,36 @@ const products=[
 ];
 app.get("/api/products",(req,res)=>{
     res.json(products);
+});
+app.post("/api/products",(req,res)=>{
+    const newid=products.length+1;
+    const newProduct={
+        id:newid,
+        ...req.body
+    };
+    products.push(newProduct);
+    res.json(newProduct);
+    
+})
+app.delete("/api/Products/:id",(req,res)=>{
+    const id=Number(req.params.id);
+    const index= products.findIndex((product)=> product.id === id);
+    if(index === -1){
+         return res.status(404).json({error:"product not found"});
+    }
+    const deletedProduct=products.splice(index,1);
+    res.json(deletedProduct[0]);
+});
+app.put("/api/products/:id",(req,res)=>{
+    const id=Number(req.params.id);
+    const index=products.findIndex((product)=>product.id ===id);
+    if(index === -1){
+
+    return res.status(404).json({error:"product not found"});
+    }
+    const updatedProduct={...products[index],...req.body};
+    products[index]=updatedProduct;
+    res.json(updatedProduct);
 });
 app.get("/",(req,res)=>{res.send('backend server is running')});
 app.listen(PORT,()=>{console.log(`server is running on the port ${PORT}`)});
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,11 @@
 
 import { useEffect,useState } from "react";
+import "./App.css";
 function App(){
   const[products,setProducts]=useState([]);
+  const[name,setname]=useState("");
+  const[price,setprice]=useState("");
+  const[editid,seteditid]=useState(null);
   useEffect(()=>{
     fetch("http://localhost:5000/api/products")
     .then((response)=>response.json())
@@ -10,18 +14,88 @@ function App(){
     });
     
   },[]);
+  const addProducts=()=>{
+    const product={name:name,price:Number(price)};
+    fetch("http://localhost:5000/api/products",{
+      method:"POST",
+      headers:{"content-type":"application/json"},
+      body:JSON.stringify(product)
+    })
+    .then((response)=>response.json())
+    .then((data)=>{
+      console.log(data); 
+      setProducts([...products,data]);  
+      setname("");
+      setprice("");                 
+    });
+
+  };
+  const deleteProduct=(id)=>{
+    fetch(`http://localhost:5000/api/products/${id}`,{
+      method:"DELETE"
+
+    })
+    .then((responce)=>responce.json())
+    .then((data)=>{
+      console.log(data);
+      setProducts(products.filter((product)=>product.id !== id));
+    });
+   
+  
+  };
+
+  const updateProduct=(id,updatedProduct)=>{
+    fetch(`http://localhost:5000/api/products/${id}`,{
+      method:"PUT",
+      headers:{"content-type":"application/json"},
+      body:JSON.stringify(updatedProduct)
+    })
+    .then((response)=>response.json())
+    .then((data)=>{
+      console.log(data);
+      setProducts(products.map((product)=>product.id === id ?{...product,...data}:product));
+
+    });
+  };
   return (
-    <dev> 
+    <div> 
       <h1>EC</h1>
+      <input type="text" placeholder="name" value={name} onChange={(e)=>setname(e.target.value)}/>
+      <input type="number" placeholder="price" value={price} onChange={(e)=>setprice(e.target.value)}/>
+      <button onClick={addProducts}>Add Product</button>
+      {editid != null && (
+      <button onClick={()=>{
+        updateProduct(editid,{
+          name:name,
+          price:Number(price)
+        });
+        setname("");
+        setprice("");
+        seteditid(null);
+      }}>Save</button>
+     )}
+      <div className="products">
       {products.map((products)=>{
         return(
-          <div key={products.id}>
+          <div className ="product-card" key={products.id}>
           <h2>{products.name}</h2>
           <p>₹{products.price}</p>
+          <button>Add to Cart</button>
+          <button onClick={()=>
+            deleteProduct(products.id)}>Delete </button>
+          <button onClick={()=>{
+              setname(products.name);
+              setprice(products.price);
+              seteditid(products.id);
+
+          }}>Update</button>
+            
           </div>
         );
       })}
-      </dev>
+      </div>
+      </div>
+
     );
 
 }
