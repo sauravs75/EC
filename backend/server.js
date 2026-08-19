5c157aba5ff0c23556eefa0438a9bba741e16014
Saurav Suman <sauravs95088@gmail.com>
2026-08-20 02:25:16 +0530

    Errorhandling

--- backend/server.js
+++ backend/server.js
@@ -21,10 +21,21 @@ const products=[
         price:50000
     }
 ];
+// app.get("/api/products",(req,res)=>{
+//     res.json(products);
+//     res.status(200).json(products);
+// });
+
 app.get("/api/products",(req,res)=>{
-    res.json(products);
+    try{
+        res.status(200).json(products);
+    }catch(error){
+        res.status(500).json({error:"internal server error"});
+    }
 });
+
 app.post("/api/products",(req,res)=>{
+    try{
     const newid=products.length+1;
     const newProduct={
         id:newid,
@@ -32,18 +43,33 @@ app.post("/api/products",(req,res)=>{
     };
     products.push(newProduct);
     res.json(newProduct);
-    
-})
+    res.status(201).json(newProduct);
+}catch(error){
+    res.status(500).json({
+        error:"internal server error"
+    });
+}
+});
+
 app.delete("/api/Products/:id",(req,res)=>{
+    try{
     const id=Number(req.params.id);
     const index= products.findIndex((product)=> product.id === id);
     if(index === -1){
          return res.status(404).json({error:"product not found"});
     }
     const deletedProduct=products.splice(index,1);
-    res.json(deletedProduct[0]);
+    res.status(200).json(deletedProduct[0]);
+}catch(error){
+    res.status(500).json({
+        error:"internal server error"
+    });
+}
+
 });
 app.put("/api/products/:id",(req,res)=>{
+    try{
+
     const id=Number(req.params.id);
     const index=products.findIndex((product)=>product.id ===id);
     if(index === -1){
@@ -53,6 +79,12 @@ app.put("/api/products/:id",(req,res)=>{
     const updatedProduct={...products[index],...req.body};
     products[index]=updatedProduct;
     res.json(updatedProduct);
+    res.status(200).json(updatedProduct);
+}catch(error){
+    res.status(500).json({
+        error:"internal server error"
+    });
+}
 });
 app.get("/",(req,res)=>{res.send('backend server is running')});
 app.listen(PORT,()=>{console.log(`server is running on the port ${PORT}`)});
